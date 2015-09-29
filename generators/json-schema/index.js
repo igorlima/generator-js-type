db936434c906777886ac66ddd407154a9e39f476
Igor Lima <ilima@fanatics.com>
2015-09-28 23:12:51 -0700

    Create JS6 classes from json schema

---
+++ generators/json-schema/index.js
@@ -0,0 +1,51 @@
+var generators = require('yeoman-generator');
+var _ = require('lodash');
+var fs = require('fs');
+var path = require('path');
+
+module.exports = generators.Base.extend( {
+
+  // The name `constructor` is important here
+  constructor: function () {
+    // Calling the super constructor is important so our generator is correctly set up
+    generators.Base.apply(this, arguments);
+
+    // This makes `classname` a required argument.
+    this.argument( 'filepath', { type: String, required: true, desc: 'json-schema file path' } );
+    // And you can then access it later on this way; e.g. CamelCased
+    this.filepath = _.camelCase( this.filepath );
+
+    // Next, add your custom code
+    this.option( 'coffee' ); // This method adds support for a `--coffee` flag
+
+  },
+
+  writing: function () {
+    var done = this.async();
+    this.log('hello')
+    fs.readFile( this.filepath, 'utf8', function ( err, data ) {
+      if ( err ) throw err;
+
+      var jsonSchema = JSON.parse( data );
+      var ext = path.extname( this.filepath );
+      var filename = path.basename( this.filepath, ext );
+
+      this.fs.copyTpl(
+        this.templatePath('../../app/templates/class.js'),
+        this.destinationPath( filename + '.js' ),
+        {
+          classname: filename,
+          attributes: _.map( jsonSchema.properties, function(property, propertyName) {
+            return {
+              name: propertyName,
+              type: property.type
+            };
+          } )
+        }
+      );
+
+      done();
+    }.bind(this) );
+  }
+
+} );
