1fe6088298b41ec00d479a47da8e6bd56f344d83
Igor Ribeio Lima <lima.igorribeiro@gmail.com>
2015-10-08 16:57:32 -0700

    Fix issue

--- test/generator-json-schema_spec.js
+++ test/generator-json-schema_spec.js
@@ -39,12 +39,12 @@ describe('generator json schema with class template 1', function () {
     })
 
     it('have a class definition in the file', function () {
-      assert.fileContent('Song.js', 'class Song {')
+      assert.fileContent('song.js', 'class Song {')
     })
 
     it('have a constructor definition in the class', function () {
       assert.fileContent(
-        'Song.js',
+        'song.js',
         new RegExp([
           'constructor[(]',
           ' id:number, title:string, artist:string, genre:string ',
@@ -54,20 +54,20 @@ describe('generator json schema with class template 1', function () {
     })
 
     it('have id variable declared', function () {
-      assert.fileContent('Song.js', 'id: number;')
+      assert.fileContent('song.js', 'id: number;')
     })
 
     it('have title variable declared', function () {
-      assert.fileContent('Song.js', 'title: string;')
+      assert.fileContent('song.js', 'title: string;')
     })
 
     it('have genre variable declared', function () {
-      assert.fileContent('Song.js', 'genre: string;')
+      assert.fileContent('song.js', 'genre: string;')
     })
 
     it('need to be validated by FlowType', function (done) {
       runFlowType(this.generator_temporary_dir, function (code) {
-        assert.file(path.join(__dirname, 'tmp/Song.js'))
+        assert.file(path.join(__dirname, 'tmp/song.js'))
         assert.strictEqual(code, 0)
         done()
       })
@@ -96,24 +96,24 @@ describe('generator json schema with class template 1', function () {
     })
 
     it('import Song class', function () {
-      assert.fileContent('SongList.js', 'import { Song } from "./Song"')
+      assert.fileContent('songList.js', 'import { Song } from "./Song"')
     })
 
     it('have song list variable declared', function () {
-      assert.fileContent('SongList.js', 'songs: Array<Song>;')
+      assert.fileContent('songList.js', 'songs: Array<Song>;')
     })
 
     it('have sont list arguments in the constructor', function () {
       assert.fileContent(
-        'SongList.js',
+        'songList.js',
         /constructor[(] songs:Array<Song> [)] {/
       )
     })
 
     it('need to be validated by FlowType', function (done) {
       runFlowType(this.generator_temporary_dirs, function (code) {
-        assert.file(path.join(__dirname, 'tmp/Song.js'))
-        assert.file(path.join(__dirname, 'tmp/SongList.js'))
+        assert.file(path.join(__dirname, 'tmp/song.js'))
+        assert.file(path.join(__dirname, 'tmp/songList.js'))
         assert.strictEqual(code, 0)
         done()
       })
@@ -134,24 +134,24 @@ describe('generator json schema with class template 1', function () {
     })
 
     it('should not import string class', function () {
-      assert.noFileContent('Product.js', 'import')
+      assert.noFileContent('product.js', 'import')
     })
 
     it('should not contain space in a variable', function () {
-      assert.fileContent('Product.js', 'onSale: Array<string>;')
+      assert.fileContent('product.js', 'onSale: Array<string>;')
     })
 
     it('have camel case variables', function () {
-      assert.fileContent('Product.js', 'hasDynamicImages: boolean;')
+      assert.fileContent('product.js', 'hasDynamicImages: boolean;')
     })
 
     it('convert `integer` to `number` due --convert options', function () {
-      assert.fileContent('Product.js', 'productId: number;')
+      assert.fileContent('product.js', 'productId: number;')
     })
 
     it('need to be validated by FlowType', function (done) {
       runFlowType(this.generator_temporary_dir, function (code) {
-        assert.file(path.join(__dirname, 'tmp/Product.js'))
+        assert.file(path.join(__dirname, 'tmp/product.js'))
         assert.strictEqual(code, 0)
         done()
       })
@@ -173,20 +173,20 @@ describe('generator json schema with class template 1', function () {
       })
 
       it('should NOT import any class', function () {
-        assert.noFileContent('SearchFilter.js', 'import')
+        assert.noFileContent('searchFilter.js', 'import')
       })
 
       it('filters is an array of objects', function () {
-        assert.fileContent('SearchFilter.js', 'filters: Array<Object>;')
+        assert.fileContent('searchFilter.js', 'filters: Array<Object>;')
       })
 
       it('sortingTemplate is an object', function () {
-        assert.fileContent('SearchFilter.js', 'sortingTemplate: Object;')
+        assert.fileContent('searchFilter.js', 'sortingTemplate: Object;')
       })
 
       it('need to be validated by FlowType', function (done) {
         runFlowType(this.generator_temporary_dir, function (code) {
-          assert.file(path.join(__dirname, 'tmp/SearchFilter.js'))
+          assert.file(path.join(__dirname, 'tmp/searchFilter.js'))
           assert.strictEqual(code, 0)
           done()
         })
@@ -209,83 +209,83 @@ describe('generator json schema with class template 1', function () {
 
       describe('SearchFilter file', function () {
         it('file exists', function () {
-          assert.file('SearchFilter.js')
+          assert.file('searchFilter.js')
         })
 
         it('import Filter class', function () {
           assert.fileContent(
-            'SearchFilter.js',
+            'searchFilter.js',
             'import { Filter } from "./Filter"')
         })
 
         it('filters is an array of filter', function () {
-          assert.fileContent('SearchFilter.js', 'filters: Array<Filter>;')
+          assert.fileContent('searchFilter.js', 'filters: Array<Filter>;')
         })
 
         it('import SortingTemplate class', function () {
           assert.fileContent(
-            'SearchFilter.js',
+            'searchFilter.js',
             'import { SortingTemplate } from "./SortingTemplate";')
         })
 
         it('sortingTemplate is SortingTemplate object', function () {
           assert.fileContent(
-            'SearchFilter.js',
+            'searchFilter.js',
             'sortingTemplate: SortingTemplate;')
         })
       })
 
       describe('SortingTemplate file', function () {
         it('file exists', function () {
-          assert.file('SortingTemplate.js')
+          assert.file('sortingTemplate.js')
         })
 
         it('import SortingFilter class', function () {
           assert.fileContent(
-            'SortingTemplate.js',
+            'sortingTemplate.js',
             'import { SortingFilter } from "./SortingFilter";')
         })
 
         it('sortingFilters is an array of SortingFilter', function () {
           assert.fileContent(
-            'SortingTemplate.js',
+            'sortingTemplate.js',
             'sortingFilters: Array<SortingFilter>;')
         })
       })
 
       describe('Filter file', function () {
         it('file exists', function () {
-          assert.file('Filter.js')
+          assert.file('filter.js')
         })
 
         it('propertyName is a string', function () {
           assert.fileContent(
-            'Filter.js',
+            'filter.js',
             'propertyName: string;')
         })
 
         it('propertyValue is a string', function () {
           assert.fileContent(
-            'Filter.js',
+            'filter.js',
             'propertyValue: string;')
         })
       })
 
       describe('SortingFilter file', function () {
         it('file exists', function () {
-          assert.file('SortingFilter.js')
+          assert.file('sortingFilter.js')
         })
 
         it('propertyName is a string', function () {
           assert.fileContent(
-            'SortingFilter.js',
+            'sortingFilter.js',
             'propertyName: string;')
         })
       })
 
       it('need to be validated by FlowType', function (done) {
         runFlowType(this.generator_temporary_dir, function (code) {
-          assert.file(path.join(__dirname, 'tmp/SearchFilter.js'))
+          assert.file(path.join(__dirname, 'tmp/searchFilter.js'))
           assert.strictEqual(code, 0)
           done()
         })
